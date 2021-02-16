587b81b116ba30f3326c6a0b7a4b8e10153f9ab5
xyovanz <xyovanz@users.noreply.github.com>
2021-02-16 21:52:09 +0300

    lesson8

---
+++ Lesson8/L8App01/Question.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L8App01
+{
+    public class Question
+    {
+        public string Text { get; set; }
+
+        public bool TrueFalse { get; set; }
+
+        #region Constructors
+
+        public Question()
+        {
+
+        }
+
+        public Question(string text, bool trueFalse)
+        {
+            Text = text;
+            TrueFalse = trueFalse;
+        }
+
+        #endregion
+    }
+}
---
+++ Lesson8/L8App02/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace L8App02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsBtnFile = new System.Windows.Forms.ToolStripDropDownButton();
+            this.tsbtnOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsbtnExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbQuestion = new System.Windows.Forms.TextBox();
+            this.btnYes = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblQuestionsCount = new System.Windows.Forms.Label();
+            this.lblScoreText = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsBtnFile});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(592, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsBtnFile
+            // 
+            this.tsBtnFile.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsBtnFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbtnOpen,
+            this.tsbtnExit});
+            this.tsBtnFile.Image = ((System.Drawing.Image)(resources.GetObject("tsBtnFile.Image")));
+            this.tsBtnFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsBtnFile.Name = "tsBtnFile";
+            this.tsBtnFile.Size = new System.Drawing.Size(49, 22);
+            this.tsBtnFile.Text = "Файл";
+            // 
+            // tsbtnOpen
+            // 
+            this.tsbtnOpen.Name = "tsbtnOpen";
+            this.tsbtnOpen.Size = new System.Drawing.Size(136, 22);
+            this.tsbtnOpen.Text = "Новая игра";
+            this.tsbtnOpen.Click += new System.EventHandler(this.tsbtnNewGame_Click);
+            // 
+            // tsbtnExit
+            // 
+            this.tsbtnExit.Name = "tsbtnExit";
+            this.tsbtnExit.Size = new System.Drawing.Size(136, 22);
+            this.tsbtnExit.Text = "Выход";
+            this.tsbtnExit.Click += new System.EventHandler(this.tsbtnExit_Click);
+            // 
+            // tbQuestion
+            // 
+            this.tbQuestion.BackColor = System.Drawing.Color.LemonChiffon;
+            this.tbQuestion.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tbQuestion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbQuestion.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbQuestion.Location = new System.Drawing.Point(0, 25);
+            this.tbQuestion.Multiline = true;
+            this.tbQuestion.Name = "tbQuestion";
+            this.tbQuestion.ReadOnly = true;
+            this.tbQuestion.Size = new System.Drawing.Size(592, 262);
+            this.tbQuestion.TabIndex = 1;
+            this.tbQuestion.Text = "Добро пожаловать в игру Верю - не верю! \r\nЧтобы начать игру - откройте файл с воп" +
+    "росами.";
+            // 
+            // btnYes
+            // 
+            this.btnYes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnYes.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnYes.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnYes.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnYes.Location = new System.Drawing.Point(3, 12);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(248, 102);
+            this.btnYes.TabIndex = 2;
+            this.btnYes.Text = "Верю";
+            this.btnYes.UseVisualStyleBackColor = false;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNo.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnNo.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnNo.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnNo.Location = new System.Drawing.Point(338, 16);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(250, 102);
+            this.btnNo.TabIndex = 3;
+            this.btnNo.Text = "Не верю";
+            this.btnNo.UseVisualStyleBackColor = false;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lblQuestionsCount);
+            this.panel1.Controls.Add(this.lblScoreText);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblScore);
+            this.panel1.Controls.Add(this.btnYes);
+            this.panel1.Controls.Add(this.btnNo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 166);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(592, 121);
+            this.panel1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(259, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 32);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Вопросов \r\nосталось:";
+            // 
+            // lblQuestionsCount
+            // 
+            this.lblQuestionsCount.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lblQuestionsCount.AutoSize = true;
+            this.lblQuestionsCount.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblQuestionsCount.Location = new System.Drawing.Point(283, 95);
+            this.lblQuestionsCount.Name = "lblQuestionsCount";
+            this.lblQuestionsCount.Size = new System.Drawing.Size(18, 18);
+            this.lblQuestionsCount.TabIndex = 7;
+            this.lblQuestionsCount.Text = "0";
+            this.lblQuestionsCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblScoreText
+            // 
+            this.lblScoreText.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lblScoreText.AutoSize = true;
+            this.lblScoreText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblScoreText.Location = new System.Drawing.Point(272, 16);
+            this.lblScoreText.Name = "lblScoreText";
+            this.lblScoreText.Size = new System.Drawing.Size(47, 20);
+            this.lblScoreText.TabIndex = 5;
+            this.lblScoreText.Text = "Счет";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2288, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "0";
+            // 
+            // lblScore
+            // 
+            this.lblScore.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblScore.Location = new System.Drawing.Point(283, 36);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(23, 23);
+            this.lblScore.TabIndex = 4;
+            this.lblScore.Text = "0";
+            this.lblScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 287);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tbQuestion);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Верю - не верю";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton tsBtnFile;
+        private System.Windows.Forms.ToolStripMenuItem tsbtnExit;
+        private System.Windows.Forms.TextBox tbQuestion;
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem tsbtnOpen;
+        private System.Windows.Forms.Label lblScoreText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblQuestionsCount;
+    }
+}
+
